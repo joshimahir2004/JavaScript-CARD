a57871ff7e37b4c1bc7389226857a1224dece045
joshimahir2004 <104302856+joshimahir2004@users.noreply.github.com>
2022-09-25 14:10:09 +0530

    Add files via upload

---
+++ 1/js.js
@@ -0,0 +1,41 @@
+// let fistName = 'MJ';
+// console.log(fistName);
+// let Name = 10;
+// console.log(Name);
+
+
+// let person = {
+//   name:'mahir joshi',
+//   number: 10
+// };
+// console.log(person);
+
+
+//array
+// let person = ["mj", " red", "blue"];
+// console.log(person[0]);
+
+// function
+// function person(name){
+ 
+//     console.log(name + "Hi " );
+// }
+// person("Mahir");
+
+// function_2
+// function square(number) {
+//     let num2 = number * number;
+//     return num2;
+// }
+// // num(3);
+// console.log(square(3));
+
+
+// ex
+
+function count() {
+  let input = document.getElementById('Input').value;
+  let lbl = document.getElementById('lbl');
+  lbl.innerText =  input;
+  // alert(lbl.innerText);
+}
